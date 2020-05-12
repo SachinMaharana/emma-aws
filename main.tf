e04a50cff2d3616719eed2cf009b1d72536b2208
sachin maharana <sachin.nicky@gmail.com>
2020-05-12 20:42:10 +0530

    subnets

--- main.tf
+++ main.tf
@@ -8,3 +8,81 @@ data "aws_ami" "ubuntu" {
   }
 }
 
+data "aws_ami" "amazonlinux" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm*"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+}
+
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+data "http" "workstation-external-ip" {
+  url = "http://ipv4.icanhazip.com"
+}
+
+
+locals {
+  workstation-external-cidr = "${chomp(data.http.workstation-external-ip.body)}/32"
+}
+
+data "aws_route53_zone" "selected" {
+  name         = "${var.hosted_zone}"
+  private_zone = false
+}
+
+######################################### RESOURCES #################
+
+resource "aws_vpc" "main" {
+  cidr_block                       = var.aws_vpc_cidr
+  enable_dns_hostnames             = true
+  assign_generated_ipv6_cidr_block = false
+
+  tags = {
+    Name    = "${var.project}-vpc"
+    Project = var.project
+    Owner   = var.owner
+  }
+}
+
+resource "aws_subnet" "public" {
+  count             = var.availability_zones
+  availability_zone = data.aws_availability_zones.available.names[count.index]
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = cidrsubnet(var.aws_vpc_cidr, 8, count.index + 11)
+
+  tags = {
+    Name      = "${var.project}-public-${count.index}"
+    Attribute = "public"
+    Project   = var.project
+    Owner     = var.owner
+  }
+
+}
+
+
+resource "aws_subnet" "private" {
+  count                   = var.availability_zones
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = cidrsubnet(var.aws_vpc_cidr, 8, count.index + 1)
+  map_public_ip_on_launch = false
+
+
+  tags = {
+    Name      = "${var.project}-private-${count.index}"
+    Attribute = "private"
+    Project   = var.project
+    Owner     = var.owner
+  }
+}
--- variables.tf
+++ variables.tf
@@ -9,3 +9,34 @@ variable "aws_profile" {
   type        = string
   default     = "default"
 }
+
+variable "hosted_zone" {
+  type        = string
+  description = "Route53 Hosted Zone for creating records (without . suffix, e.g. `sachinmaharana.dev`)"
+  default     = "sachinm.site"
+}
+
+
+variable "aws_vpc_cidr" {
+  description = "VPC CIDR block"
+  type        = string
+  default     = "10.23.0.0/16"
+}
+
+variable "owner" {
+  description = "Owner name used for tags"
+  type        = string
+  default     = "sachinm.site"
+}
+
+variable "project" {
+  description = "Project name used for tags"
+  type        = string
+  default     = "emma-aws"
+}
+
+variable "availability_zones" {
+  description = "Number of different AZs to use"
+  type        = number
+  default     = 3
+}
