bfd8e91b17bf86b791e151747828697c2523b551
sachin maharana <sachin.nicky@gmail.com>
2020-05-12 23:09:59 +0530

    sg;iam

--- main.tf
+++ main.tf
@@ -393,3 +393,433 @@ resource "aws_security_group" "worker" {
     create_before_destroy = true
   }
 }
+
+# Ingress
+resource "aws_security_group_rule" "ssh" {
+  for_each = {
+    "Etcd"    = aws_security_group.etcd.id,
+    "Masters" = aws_security_group.master.id,
+    "Workers" = aws_security_group.worker.id,
+  }
+  security_group_id        = each.value
+  type                     = "ingress"
+  from_port                = 22
+  to_port                  = 22
+  protocol                 = "tcp"
+  source_security_group_id = aws_security_group.bastion.id
+  description              = "SSH: Bastion - ${each.key}"
+}
+
+
+resource "aws_security_group_rule" "allow_ingress_on_bastion_kubectl" {
+  for_each = {
+    "MasterPrivateLB" = aws_security_group.master-private-lb.id,
+    "Masters"         = aws_security_group.master.id,
+    "Workers"         = aws_security_group.worker.id
+  }
+  security_group_id        = each.value
+  type                     = "ingress"
+  from_port                = 6443
+  to_port                  = 6443
+  protocol                 = "tcp"
+  source_security_group_id = aws_security_group.bastion.id
+  description              = "kubectl: Bastion - ${each.key}"
+}
+
+resource "aws_security_group_rule" "allow_ingress_bastion-lb_on_bastion_ssh" {
+  security_group_id        = aws_security_group.bastion.id
+  type                     = "ingress"
+  from_port                = 22
+  to_port                  = 22
+  protocol                 = "tcp"
+  source_security_group_id = aws_security_group.bastion-lb.id
+  description              = "SSH: Bastion-LB - Bastion"
+}
+
+resource "aws_security_group_rule" "allow_ingress_workstation_on_bastion-lb_ssh" {
+  security_group_id = aws_security_group.bastion-lb.id
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = [local.workstation-external-cidr]
+  description       = "SSH: Workstation - MasterPublicLB"
+}
+
+resource "aws_security_group_rule" "allow_ingress_workstation_on-master-public-lb_kubectl" {
+  security_group_id = aws_security_group.master-public-lb.id
+  type              = "ingress"
+  from_port         = 6443
+  to_port           = 6443
+  protocol          = "tcp"
+  cidr_blocks       = [local.workstation-external-cidr]
+  description       = "kubectl: Workstation - MasterPublicLB"
+}
+
+resource "aws_security_group_rule" "allow_ingress_on_master-private-lb_kubeapi" {
+  security_group_id = aws_security_group.master-private-lb.id
+  type              = "ingress"
+  from_port         = 6443
+  to_port           = 6443
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  description       = "kubeapi: ALL - MasterPrivateLB"
+}
+
+resource "aws_security_group_rule" "allow_etcd" {
+  for_each = {
+    "Masters" = aws_security_group.master.id,
+    "Etcd"    = aws_security_group.etcd.id
+  }
+  security_group_id        = aws_security_group.etcd.id
+  type                     = "ingress"
+  from_port                = 2379
+  to_port                  = 2380
+  protocol                 = "tcp"
+  source_security_group_id = each.value
+  description              = "etcd: ${each.key} - Etcds"
+}
+
+resource "aws_security_group_rule" "allow_kubectl_on_master" {
+  for_each = {
+    "MasterPublicLB"  = aws_security_group.master-public-lb.id,
+    "MasterPrivateLB" = aws_security_group.master-private-lb.id,
+    "Workers"         = aws_security_group.worker.id
+  }
+  security_group_id        = aws_security_group.master.id
+  type                     = "ingress"
+  from_port                = 6443
+  to_port                  = 6443
+  protocol                 = "tcp"
+  source_security_group_id = each.value
+  description              = "kubectl: ${each.key} - Masters"
+}
+
+resource "aws_security_group_rule" "allow_ingress_worker_on_master_all" {
+  security_group_id        = aws_security_group.master.id
+  type                     = "ingress"
+  from_port                = 0
+  to_port                  = 65535
+  protocol                 = "all"
+  source_security_group_id = aws_security_group.worker.id
+  description              = "ALL: Workers - Masters"
+}
+
+resource "aws_security_group_rule" "allow_ingress_on_worker_all" {
+  for_each = {
+    "Masters"         = aws_security_group.master.id,
+    "MasterPrivateLB" = aws_security_group.master-private-lb.id
+  }
+  security_group_id        = aws_security_group.worker.id
+  type                     = "ingress"
+  from_port                = 0
+  to_port                  = 65535
+  protocol                 = "all"
+  source_security_group_id = each.value
+  description              = "ALL: ${each.key} - Workers"
+}
+
+resource "aws_security_group_rule" "egress_all" {
+  for_each = {
+    "BastionLB"       = aws_security_group.bastion-lb.id,
+    "MasterPublicLB"  = aws_security_group.master-public-lb.id,
+    "MasterPrivateLB" = aws_security_group.master-private-lb.id,
+    "Bastion"         = aws_security_group.bastion.id,
+    "Etcds"           = aws_security_group.etcd.id,
+    "Masters"         = aws_security_group.master.id,
+    "Workers"         = aws_security_group.worker.id
+  }
+  security_group_id = each.value
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+  description       = "Egress ALL: ${each.key}"
+}
+
+resource "aws_elb" "bastion" {
+  name_prefix     = "basti-" // cannot be longer than 6 characters
+  subnets         = aws_subnet.public.*.id
+  security_groups = [aws_security_group.bastion-lb.id]
+
+  listener {
+    instance_port     = 22
+    instance_protocol = "tcp"
+    lb_port           = 22
+    lb_protocol       = "tcp"
+  }
+
+  tags = {
+    Name    = "${var.project}-bastion-lb"
+    Project = var.project
+    Owner   = var.owner
+  }
+}
+
+resource "aws_elb" "master-public" {
+  name_prefix     = "master" // cannot be longer than 6 characters
+  subnets         = aws_subnet.public.*.id
+  security_groups = [aws_security_group.master-public-lb.id]
+
+  listener {
+    instance_port     = 6443
+    instance_protocol = "tcp"
+    lb_port           = 6443
+    lb_protocol       = "tcp"
+  }
+
+  tags = {
+    Name      = "${var.project}-master--publiclb"
+    Attribute = "public"
+    Project   = var.project
+    Owner     = var.owner
+  }
+}
+
+resource "aws_elb" "master-private" {
+  name_prefix     = "master" // will be prefixed with internal -  cannot be longer than 6 characters
+  internal        = true
+  subnets         = aws_subnet.public.*.id
+  security_groups = [aws_security_group.master-private-lb.id]
+
+  listener {
+    instance_port     = 6443
+    instance_protocol = "tcp"
+    lb_port           = 6443
+    lb_protocol       = "tcp"
+  }
+
+  tags = {
+    Name     = "${var.project}-master--private-lb"
+    ttribute = "private"
+    Project  = var.project
+    Owner    = var.owner
+  }
+}
+
+resource "aws_launch_configuration" "bastion" {
+  name_prefix                 = "bastion-"
+  image_id                    = data.aws_ami.amazonlinux.id
+  instance_type               = var.bastion_instance_type
+  security_groups             = [aws_security_group.bastion.id]
+  key_name                    = var.aws_key_pair_name == null ? aws_key_pair.ssh.0.key_name : var.aws_key_pair_name
+  associate_public_ip_address = false
+  ebs_optimized               = true
+  enable_monitoring           = true
+  iam_instance_profile        = aws_iam_instance_profile.bastion.id
+
+  user_data = templatefile("${path.module}/userdata-bastion.tpl", {
+    component = "bastion"
+    domain    = var.hosted_zone
+  })
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+
+## etcd
+resource "aws_launch_configuration" "etcd" {
+  name_prefix                 = "etcd-"
+  image_id                    = data.aws_ami.ubuntu.id
+  instance_type               = var.etcd_instance_type
+  security_groups             = [aws_security_group.etcd.id]
+  key_name                    = var.aws_key_pair_name == null ? aws_key_pair.ssh.0.key_name : var.aws_key_pair_name
+  associate_public_ip_address = false
+  ebs_optimized               = true
+  enable_monitoring           = true
+  iam_instance_profile        = aws_iam_instance_profile.etcd_worker_master.id
+
+  user_data = templatefile("${path.module}/userdata.tpl", {
+    domain = var.hosted_zone
+  })
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+## Kubernetes Master
+resource "aws_launch_configuration" "master" {
+  name_prefix                 = "master-"
+  image_id                    = data.aws_ami.ubuntu.id
+  instance_type               = var.master_instance_type
+  security_groups             = [aws_security_group.master.id]
+  key_name                    = var.aws_key_pair_name == null ? aws_key_pair.ssh.0.key_name : var.aws_key_pair_name
+  associate_public_ip_address = false
+  ebs_optimized               = true
+  enable_monitoring           = true
+  iam_instance_profile        = aws_iam_instance_profile.etcd_worker_master.id
+
+  user_data = templatefile("${path.module}/userdata.tpl", {
+    domain = var.hosted_zone
+  })
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+## Kubernetes Worker
+resource "aws_launch_configuration" "worker" {
+  name_prefix                 = "worker-"
+  image_id                    = data.aws_ami.ubuntu.id
+  instance_type               = var.worker_instance_type
+  security_groups             = [aws_security_group.worker.id]
+  key_name                    = var.aws_key_pair_name == null ? aws_key_pair.ssh.0.key_name : var.aws_key_pair_name
+  associate_public_ip_address = false
+  ebs_optimized               = true
+  enable_monitoring           = true
+  iam_instance_profile        = aws_iam_instance_profile.etcd_worker_master.id
+
+  user_data = templatefile("${path.module}/userdata-worker.tpl", {
+    pod_cidr = var.pod_cidr
+    domain   = var.hosted_zone
+  })
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+
+## Bastion
+resource "aws_autoscaling_group" "bastion" {
+  max_size             = var.bastion_max_size
+  min_size             = var.bastion_min_size
+  desired_capacity     = var.bastion_size
+  force_delete         = false
+  launch_configuration = aws_launch_configuration.bastion.name
+  vpc_zone_identifier  = aws_subnet.private.*.id
+  load_balancers       = [aws_elb.bastion.id]
+
+  tags = [
+    {
+      key                 = "Name"
+      value               = "${var.project}-bastion"
+      propagate_at_launch = true
+    },
+    {
+      key                 = "Project"
+      value               = var.project
+      propagate_at_launch = true
+    },
+    {
+      key                 = "Owner"
+      value               = var.owner
+      propagate_at_launch = true
+    }
+  ]
+}
+
+## etcd
+resource "aws_autoscaling_group" "etcd" {
+  max_size             = var.etcd_max_size
+  min_size             = var.etcd_min_size
+  desired_capacity     = var.etcd_size
+  force_delete         = true
+  launch_configuration = aws_launch_configuration.etcd.name
+  vpc_zone_identifier  = aws_subnet.private.*.id
+
+  tags = [
+    {
+      key                 = "Name"
+      value               = "${var.project}-etcd"
+      propagate_at_launch = true
+    },
+    {
+      key                 = "Project"
+      value               = var.project
+      propagate_at_launch = true
+    },
+    {
+      key                 = "Owner"
+      value               = var.owner
+      propagate_at_launch = true
+    }
+  ]
+}
+
+## Kubernetes Master
+resource "aws_autoscaling_group" "master" {
+  max_size             = var.master_max_size
+  min_size             = var.master_min_size
+  desired_capacity     = var.master_size
+  force_delete         = true
+  launch_configuration = aws_launch_configuration.master.name
+  vpc_zone_identifier  = aws_subnet.private.*.id
+  load_balancers       = [aws_elb.master-public.id, aws_elb.master-private.id]
+
+  tags = [
+    {
+      key                 = "Name"
+      value               = "${var.project}-k8s-master"
+      propagate_at_launch = true
+    },
+    {
+      key                 = "Project"
+      value               = var.project
+      propagate_at_launch = true
+    },
+    {
+      key                 = "Owner"
+      value               = var.owner
+      propagate_at_launch = true
+    }
+  ]
+}
+
+## Kubernetes Worker
+resource "aws_autoscaling_group" "worker" {
+  max_size             = var.worker_max_size
+  min_size             = var.worker_min_size
+  desired_capacity     = var.worker_size
+  force_delete         = true
+  launch_configuration = aws_launch_configuration.worker.name
+  vpc_zone_identifier  = aws_subnet.private.*.id
+
+  tags = [
+    {
+      key                 = "Name"
+      value               = "${var.project}-k8s-worker"
+      propagate_at_launch = true
+    },
+    {
+      key                 = "Project"
+      value               = var.project
+      propagate_at_launch = true
+    },
+    {
+      key                 = "Owner"
+      value               = var.owner
+      propagate_at_launch = true
+    }
+  ]
+}
+
+resource "aws_route53_record" "bastion-public" {
+  zone_id = data.aws_route53_zone.selected.id
+  name    = "bastion"
+  type    = "A"
+
+  alias {
+    evaluate_target_health = false
+    name                   = aws_elb.bastion.dns_name
+    zone_id                = aws_elb.bastion.zone_id
+  }
+}
+
+## Kubernetes Master for remote kubectl access
+resource "aws_route53_record" "master_lb-public" {
+  zone_id = data.aws_route53_zone.selected.id
+  name    = "kube"
+  type    = "A"
+
+  alias {
+    evaluate_target_health = false
+    name                   = aws_elb.master-public.dns_name
+    zone_id                = aws_elb.master-public.zone_id
+  }
+}
--- variables.tf
+++ variables.tf
@@ -52,3 +52,107 @@ variable "aws_key_pair_name" {
   type        = string
   default     = null
 }
+
+variable "pod_cidr" {
+  description = "The first two octets for the Pod network CIDR (used in Worker UserData to generate POD_CIDR envvar)"
+  type        = string
+  default     = "10.200"
+}
+variable "bastion_max_size" {
+  description = "Maximum number of EC2 instances for Bastion AutoScalingGroup"
+  type        = number
+  default     = 1
+}
+
+
+variable "bastion_min_size" {
+  description = "Minimum number of EC2 instances for Bastion AutoScalingGroup"
+  type        = number
+  default     = 1
+}
+
+variable "bastion_size" {
+  description = "Desired number of EC2 instances for Bastion AutoScalingGroup"
+  type        = number
+  default     = 1
+}
+
+variable "etcd_max_size" {
+  description = "Maximum number of EC2 instances for etcd AutoScalingGroup"
+  type        = number
+  default     = 3
+}
+
+variable "etcd_min_size" {
+  description = "Minimum number of EC2 instances for etcd AutoScalingGroup"
+  type        = number
+  default     = 3
+}
+
+variable "etcd_size" {
+  description = "Desired number of EC2 instances for etcd AutoScalingGroup"
+  type        = number
+  default     = 3
+}
+
+variable "master_max_size" {
+  description = "Maximum number of EC2 instances for K8s Master AutoScalingGroup"
+  type        = number
+  default     = 3
+}
+
+variable "master_min_size" {
+  description = "Minimum number of EC2 instances for K8s Master AutoScalingGroup"
+  type        = number
+  default     = 3
+}
+
+
+
+variable "master_size" {
+  description = "Desired number of EC2 instances for K8s Master AutoScalingGroup"
+  type        = number
+  default     = 3
+}
+
+variable "worker_max_size" {
+  description = "Maximum number of EC2 instances for K8s Worker AutoScalingGroup"
+  type        = number
+  default     = 3
+}
+
+variable "worker_min_size" {
+  description = "Minimum number of EC2 instances for K8s Worker AutoScalingGroup"
+  type        = number
+  default     = 3
+}
+
+variable "worker_size" {
+  description = "Desired number of EC2 instances for K8s Worker AutoScalingGroup"
+  type        = number
+  default     = 3
+}
+
+variable "bastion_instance_type" {
+  description = "EC2 instance type for Bastion Host"
+  type        = string
+  default     = "t3a.small"
+}
+
+variable "etcd_instance_type" {
+  description = "EC2 instance type for etcd instances"
+  type        = string
+  default     = "t3a.small"
+}
+
+variable "master_instance_type" {
+  description = "EC2 instance type for K8s master instances"
+  type        = string
+  default     = "t3a.small"
+}
+
+variable "worker_instance_type" {
+  description = "EC2 instance type for K8s worker instances"
+  type        = string
+  default     = "t3a.small"
+}
