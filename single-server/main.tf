6d5a1b7d45a620302bf3e968ea91d0a62b4800a4
sachin maharana <sachin.nicky@gmail.com>
2020-05-15 22:53:31 +0530

    single-server

---
+++ single-server/main.tf
@@ -0,0 +1,5 @@
+
+resource "aws_instance" "ss" {
+  ami           = "ami-0c55b159cbfafe1f0"
+  instance_type = "t2.micro"
+}
---
+++ single-server/variables.tf
@@ -0,0 +1,11 @@
+variable "aws_region" {
+  type        = string
+  description = "AWS Region this resource will be deployed"
+  default     = "us-east-2"
+}
+
+variable "aws_profile" {
+  type        = string
+  description = "AWS Profile to be used"
+  default     = "default"
+}
